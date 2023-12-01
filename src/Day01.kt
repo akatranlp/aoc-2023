50722bc737bfa37932be8bfcbfd37d64f1fb78c7
Fabian Petersen <fabian@nf-petersen.de>
2023-12-01 20:22:46 +0100

    Implement Part2 of Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -7,13 +7,60 @@ fun main() {
         }
     }
 
+    val lookupTable = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+    )
+
+    fun findFirstDigit(input: String): Int {
+        for (i in input.indices) {
+            if (input[i].isDigit()) {
+                return input[i].digitToInt()
+            }
+            val sub = input.substring(i)
+            for (num in lookupTable.keys) {
+                if (sub.startsWith(num)) {
+                    return lookupTable[num]!!
+                }
+            }
+        }
+        return 0
+    }
+
+    fun findLastDigit(input: String): Int {
+        for (i in input.indices.reversed()) {
+            if (input[i].isDigit()) {
+                return input[i].digitToInt()
+            }
+            val sub = input.substring(0, i + 1)
+            for (num in lookupTable.keys) {
+                if (sub.endsWith(num)) {
+                    return lookupTable[num]!!
+                }
+            }
+        }
+        return 0
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.fold(0) { acc, s ->
+            val firstDigit = findFirstDigit(s)
+            val lastDigit = findLastDigit(s)
+            val sum = firstDigit * 10 + lastDigit
+            sum + acc
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 142)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
