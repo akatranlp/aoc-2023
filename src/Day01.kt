ebd92401b775ab30c605317ce37d8ce0eeb36d97
Fabian Petersen <fabian@nf-petersen.de>
2023-12-01 21:23:55 +0100

    Refactor Part2 the last time

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,66 +1,38 @@
 fun main() {
     fun part1(input: List<String>): Int {
         return input.fold(0) { acc, s ->
-            s.filter { it.isDigit() }.let {
-                (it.first() + "" + it.last()).toInt()
-            } + acc
+            s.filter { it.isDigit() }.map { it.digitToInt() }.let {
+                it.first() * 10 + it.last() + acc
+            }
         }
     }
 
     val lookupTable = mapOf(
-        "one" to 1,
-        "two" to 2,
-        "three" to 3,
-        "four" to 4,
-        "five" to 5,
-        "six" to 6,
-        "seven" to 7,
-        "eight" to 8,
-        "nine" to 9
+        "one" to '1',
+        "two" to '2',
+        "three" to '3',
+        "four" to '4',
+        "five" to '5',
+        "six" to '6',
+        "seven" to '7',
+        "eight" to '8',
+        "nine" to '9'
     )
 
     fun part2(input: List<String>): Int {
-        return input.fold(0) { acc, s ->
-            var firstDigit = -1
-            var lastDigit = -1
-            var leftIndex = 0
-            var rightIndex = s.length
+        return input.map {
+            it.mapIndexed { i, c ->
+                val sub = it.substring(i)
+                var returnValue = c
 
-            loop@ while (leftIndex != rightIndex) {
-                val sub = s.substring(leftIndex, rightIndex)
-                if (firstDigit == -1) {
-                    if (sub.first().isDigit()) {
-                        firstDigit = sub.first().digitToInt()
-                    } else {
-                        for (num in lookupTable.keys) {
-                            if (sub.startsWith(num)) {
-                                firstDigit = lookupTable[num]!!
-                                continue@loop
-                            }
-                        }
-                        leftIndex++
+                for (num in lookupTable.keys) {
+                    if (sub.startsWith(num)) {
+                        returnValue = lookupTable[num]!!
                     }
                 }
-                if (lastDigit == -1) {
-                    if (sub.last().isDigit()) {
-                        lastDigit = sub.last().digitToInt()
-                    } else {
-                        for (num in lookupTable.keys) {
-                            if (sub.endsWith(num)) {
-                                lastDigit = lookupTable[num]!!
-                                continue@loop
-                            }
-                        }
-                        rightIndex--
-                    }
-                }
-                if (firstDigit != -1 && lastDigit != -1) {
-                    break
-                }
-            }
-            val sum = firstDigit * 10 + lastDigit
-            sum + acc
-        }
+                returnValue
+            }.toString()
+        }.let { part1(it) }
     }
 
     // test if implementation meets criteria from the description, like:
