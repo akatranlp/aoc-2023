a7acbabeab53d3aaccf1450e5299925485ad23a7
Fabian Petersen <fabian@nf-petersen.de>
2023-12-04 07:34:25 +0100

    Start template for Day 4

---
+++ src/Day04.kt
@@ -0,0 +1,20 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = """
+    """.trimIndent().lines()
+    check(part1(testInput) == 1)
+
+    check(part2(testInput) == 1)
+
+    val input = readInput("Day04")
+    part1(input).println()
+    part2(input).println()
+}
