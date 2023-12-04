2e6858760b7a1042efadff3d478639581a0693b9
Fabian Petersen <fabian@nf-petersen.de>
2023-12-04 07:37:15 +0100

    Implement Part1 of Day4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,6 +1,25 @@
+import kotlin.math.pow
+
+data class Card(val id: Int, val winningNumbers: Set<Int>, val myNumbers: List<Int>)
+
 fun main() {
+    fun parseCards(input: List<String>): List<Card> {
+        return input.mapIndexed { i, s ->
+            s.split(":")[1].split("|").let {
+                val winningNumbers = it[0].split(" ").filter { it != "" }.map { it.toInt() }.toSet()
+                val myNumbers = it[1].split(" ").filter { it != "" }.map { it.toInt() }
+                Card(i + 1, winningNumbers, myNumbers)
+            }
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val cards = parseCards(input)
+        return cards.sumOf { card ->
+            card.myNumbers.mapNotNull { if (it in card.winningNumbers) it else null }.let {
+                (2.toDouble().pow((it.size - 1).toDouble())).toInt()
+            }
+        }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,8 +28,14 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = """
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
     """.trimIndent().lines()
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 13)
 
     check(part2(testInput) == 1)
 
