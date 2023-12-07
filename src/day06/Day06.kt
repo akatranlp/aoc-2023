fa822cc8b20c89023de30a2b58fa2bdc9d9435f6
Fabian Petersen <fabian@nf-petersen.de>
2023-12-07 10:00:52 +0100

    Implement Part1 of Day6

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -1,10 +1,29 @@
-package day05
+package day06
 
 import utils.readInput
 
+data class Race(val time: Int, val distance: Int)
+
 fun main() {
+    fun parseInput(input: List<String>): List<Race> {
+        val (timeLine, distanceLine) = input
+        val times = timeLine.drop(5).split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+        val distances = distanceLine.drop(9).split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+
+        return times.indices.map {
+            Race(times[it], distances[it])
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val races = parseInput(input)
+
+        return races.map { race ->
+            // it mirrors so we only need to check the first half
+            (0..race.time).map {
+                it * (race.time - it)
+            }.count { it > race.distance }
+        }.reduce { acc, i -> acc * i }
     }
 
     fun part2(input: List<String>): Int {
@@ -13,11 +32,13 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = """
+        Time:      7  15   30
+        Distance:  9  40  200
     """.trimIndent().lines()
-    check(testInput.let(::part1) == 1)
-    check(testInput.let(::part2) == 1)
+    check(testInput.let(::part1) == 288)
+    // check(testInput.let(::part2) == 1)
 
     val input = readInput("day06/Day06")
-    input.let(::part1).also(::println).let { check(it == 0) }
+    input.let(::part1).also(::println).let { check(it == 211904) }
     input.let(::part2).also(::println).let { check(it == 0) }
 }
