61fa4c18dd86eacf17f9cd8ed94dbef90843433f
Fabian Petersen <fabian@nf-petersen.de>
2023-12-07 10:56:26 +0100

    Start template for days 7 and 8

---
+++ src/day07/Day07.kt
@@ -0,0 +1,23 @@
+package day07
+
+import utils.readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = """
+    """.trimIndent().lines()
+    check(testInput.let(::part1) == 1)
+    check(testInput.let(::part2) == 1)
+
+    val input = readInput("day06/Day06")
+    input.let(::part1).also(::println).let { check(it == 0) }
+    input.let(::part2).also(::println).let { check(it == 0) }
+}
